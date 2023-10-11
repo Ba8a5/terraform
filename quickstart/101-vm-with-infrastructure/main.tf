469bceb4427efad8a0ef260c20fb5d2a8a2afcd2
Bastien Rompen <73058355+Ba8a5@users.noreply.github.com>
2023-10-11 17:34:35 +0200

    Update main.tf

--- quickstart/101-vm-with-infrastructure/main.tf
+++ quickstart/101-vm-with-infrastructure/main.tf
@@ -87,7 +87,7 @@ resource "azurerm_storage_account" "my_storage_account" {
   resource_group_name      = azurerm_resource_group.rg.name
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  # allow_nested_items_to_be_public = false
+  allow_nested_items_to_be_public = false
 }
 
 # Create virtual machine
